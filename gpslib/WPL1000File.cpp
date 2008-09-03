0048dbca80f845f5dee49894e7794947464ed7af
Oliver Lau <oliver@ersatzworld.net>
2008-09-03 11:15:25 -0100

    Interim-Check-in.

--- gpslib/WPL1000File.cpp
+++ gpslib/WPL1000File.cpp
@@ -18,20 +18,35 @@ namespace GPS {
     { /* ... */ }
 
 
-#define readWPL1000TrackpointData(_Stream, _W) _Stream.read(reinterpret_cast<char*>(&_W), sizeof(_W))
+    template <typename T>
+    inline bool readWPL1000TrackpointData(fstream& fs, T* data) 
+    {
+        fs.read(reinterpret_cast<char*>(data), sizeof(T));
+        bool bad = fs.bad();
+        bool eof = fs.eof();
+        return bad;
+    }
+    
 
     int WPL1000Trackpoint::readFrom(fstream& fs)
     {
-        readWPL1000TrackpointData(fs, _Type);
-        readWPL1000TrackpointData(fs, _Unknown);
-        readWPL1000TrackpointData(fs, _T);
-        readWPL1000TrackpointData(fs, _WPL1000lat);
-        readWPL1000TrackpointData(fs, _WPL1000lon);
-        readWPL1000TrackpointData(fs, _WPL1000ele);
+        bool eof;
+        eof = readWPL1000TrackpointData<unsigned char>(fs, &_Type);
+        if (eof) return WPL1000Trackpoint::READ_EOF;
+        eof = readWPL1000TrackpointData<unsigned char>(fs, &_Unknown);
+        if (eof) return WPL1000Trackpoint::READ_EOF;
+        eof = readWPL1000TrackpointData<GPS::WPL1000Time>(fs, &_T);
+        if (eof) return WPL1000Trackpoint::READ_EOF;
+        eof = readWPL1000TrackpointData<unsigned int>(fs, &_WPL1000lat);
+        if (eof) return WPL1000Trackpoint::READ_EOF;
+        eof = readWPL1000TrackpointData<unsigned int>(fs, &_WPL1000lon);
+        if (eof) return WPL1000Trackpoint::READ_EOF;
+        eof = readWPL1000TrackpointData<unsigned short>(fs, &_WPL1000ele);
+        if (eof) return WPL1000Trackpoint::READ_EOF;
         if (_Type != CONTAINS_TRACKPOINT_DATA)
         {
-            setLatitude((double) _WPL1000lat / 10000000.0f);
-            setLongitude((double) _WPL1000lon / 10000000.0f);
+            setLatitude((double) _WPL1000lat / 10000000.0);
+            setLongitude((double) _WPL1000lon / 10000000.0);
             setElevation((double) _WPL1000ele);
             setTimestamp(Timestamp(2000 + _T.Y, _T.m, _T.d, _T.h, _T.i, _T.s));
             return READ_OK;
@@ -54,12 +69,14 @@ namespace GPS {
             Trackpoint* trkpt = new Trackpoint(point);
             if ((point.type() == WPL1000Trackpoint::TRACK_START) && (_Trk->points().size() > 0))
             {
+                _Trk->setName(_Trk->startTimestamp().toString());
                 _Trk->postProcess();
                 addTrack(_Trk);
             }
             _Trk->append(trkpt);
         }
         nvpipe.close();
+        _Trk->setName(_Trk->startTimestamp().toString());
         _Trk->postProcess();
         return 0;
     }
@@ -67,7 +84,7 @@ namespace GPS {
 
     errno_t WPL1000File::write(const std::string& /* filename */, bool /* onlyKept */)
     {
-        // do nothing
+        // it's futile to write a track to a NVPIPE.DAT file
         return 0;
     }
 };
