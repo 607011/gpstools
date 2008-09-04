c1b4134021e0a8c911fd16e909d2cb7570cac89d
Oliver Lau <oliver@ersatzworld.net>
2008-09-04 14:25:09 -0100

    gpslib: Stochastics-Klasse im Namensraum GPS untergebracht.

--- gpslib/Stochastics.cpp
+++ gpslib/Stochastics.cpp
@@ -7,23 +7,28 @@
 
 #include "Stochastics.h"
 
-void Stochastics::calculate(void)
-{
-    if (N() < 2)
-        return;
-    // Messwerte werden nur sortiert, um den Median bestimmen zu können
-    sort(_Values.begin(), _Values.end());
-    _Median = _Values.at(N() / 2);
-    _Min = _Values.front();
-    _Max = _Values.back();
-    _Sum = 0.0;
-    _SumSquared = 0.0;
-    for (std::vector<double>::const_iterator i = _Values.begin(); i != _Values.end(); ++i)
+namespace GPS {
+
+    void Stochastics::calculate(void)
     {
-        _Sum += (*i);
-        _SumSquared += (*i) * (*i);
+        if (N() < 2)
+            return;
+        // Messwerte werden nur sortiert, um den Median bestimmen zu können
+        sort(_Values.begin(), _Values.end());
+        _Median = _Values.at(N() / 2);
+        _Min = _Values.front();
+        _Max = _Values.back();
+        _Sum = 0.0;
+        _SumSquared = 0.0;
+        for (std::vector<double>::const_iterator i = _Values.begin(); i != _Values.end(); ++i)
+        {
+            _Sum += (*i);
+            _SumSquared += (*i) * (*i);
+        }
+        _Average = _Sum / N();
+        _Variance = _SumSquared / N();
+        _StandardDeviation = sqrt(_Variance);
     }
-    _Average = _Sum / N();
-    _Variance = _SumSquared / N();
-    _StandardDeviation = sqrt(_Variance);
-}
+
+
+};
--- gpslib/Stochastics.h
+++ gpslib/Stochastics.h
@@ -8,63 +8,66 @@
 #include <cstdlib>
 #include <vector>
 
+namespace GPS {
 
-/// Die Klasse Stochastics stellt einfache Routinen zur statistischen Auswertung von Messwerten bereit.
-class Stochastics {
-private:
-    std::vector<double> _Values;
-    double _Sum;
-    double _SumSquared;
-    double _Average;
-    double _Median;
-    double _Variance;
-    double _StandardDeviation;
-    double _Min;
-    double _Max;
+    /// Die Klasse Stochastics stellt einfache Routinen zur statistischen Auswertung von Messwerten bereit.
+    class Stochastics {
+    private:
+        std::vector<double> _Values;
+        double _Sum;
+        double _SumSquared;
+        double _Average;
+        double _Median;
+        double _Variance;
+        double _StandardDeviation;
+        double _Min;
+        double _Max;
 
-public:
-    /// Konstruktor.
-    Stochastics(void)
-    { /* ... */ }
+    public:
+        /// Konstruktor.
+        Stochastics(void)
+        { /* ... */ }
 
-    /// Einen Messwerte zur Liste mit den Messungen hinzufügen.
-    inline void add(double val)
-    { 
-        _Values.push_back(val);
-    }
+        /// Einen Messwerte zur Liste mit den Messungen hinzufügen.
+        inline void add(double val)
+        { 
+            _Values.push_back(val);
+        }
 
-    /// Die Summe der Messwerte zurückgeben.
-    inline double sum(void)const  { return _Sum; }
+        /// Die Summe der Messwerte zurückgeben.
+        inline double sum(void)const  { return _Sum; }
 
-    /// Die Summe der quadrierten Messwerte zurückgeben.
-    inline double sumSquared(void) const { return _SumSquared; }
+        /// Die Summe der quadrierten Messwerte zurückgeben.
+        inline double sumSquared(void) const { return _SumSquared; }
 
-    /// Den arithmetischen Mittelwert über die Messwerte zurückgeben.
-    inline double average(void) const { return _Average; }
+        /// Den arithmetischen Mittelwert über die Messwerte zurückgeben.
+        inline double average(void) const { return _Average; }
 
-    /// Die Varianz der Messwerte zurückgeben.
-    inline double variance(void) const { return _Variance; }
+        /// Die Varianz der Messwerte zurückgeben.
+        inline double variance(void) const { return _Variance; }
 
-    /// Die Standardabweichung der Messwerte zurückgeben.
-    inline double standardDeviation(void) const { return _StandardDeviation; }
+        /// Die Standardabweichung der Messwerte zurückgeben.
+        inline double standardDeviation(void) const { return _StandardDeviation; }
 
-    /// Den Median der Messwerte zurückgeben.
-    inline double median(void) const { return _Median; }
+        /// Den Median der Messwerte zurückgeben.
+        inline double median(void) const { return _Median; }
 
-    /// Den Wert des kleinsten Messwerts zurückgeben.
-    inline double min(void) const { return _Min; }
+        /// Den Wert des kleinsten Messwerts zurückgeben.
+        inline double min(void) const { return _Min; }
 
-    /// Den Wert des größten Messwerts zurückgeben.
-    inline double max(void) const { return _Max; }
+        /// Den Wert des größten Messwerts zurückgeben.
+        inline double max(void) const { return _Max; }
 
-    /// Die Anzahl der Messwerte zurückgeben.
-    inline size_t N(void) const { return _Values.size(); }
+        /// Die Anzahl der Messwerte zurückgeben.
+        inline size_t N(void) const { return _Values.size(); }
 
-    /// Die Liste der Messwerte zurückgeben.
-    inline const std::vector<double>& values(void) { return _Values; }
+        /// Die Liste der Messwerte zurückgeben.
+        inline const std::vector<double>& values(void) { return _Values; }
 
-    /// Statistische Berechnungen über die Messwerte durchführen.
-    void calculate(void);
+        /// Statistische Berechnungen über die Messwerte durchführen.
+        void calculate(void);
+    };
+
 };
 
 #endif //  __STOCHASTICS_H_
