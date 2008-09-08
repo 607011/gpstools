d242603535cb60b67a5f9dcfc505508460b41eec
Oliver Lau <oliver@ersatzworld.net>
2008-09-08 20:45:56 +0200

    Qeo: Rudimente fuer Geotagging-Fenster neu.

---
+++ Qeo/GeoImageModel.cpp
@@ -0,0 +1,77 @@
+// $Id$
+// Copyright (c) 2008 Oliver Lau <oliver@ersatzworld.net>
+// Alle Rechte vorbehalten.
+
+#include "GeoImageModel.h"
+#include <QStringList>
+
+
+GeoImageModel::GeoImageModel(QObject *parent)
+    : QAbstractListModel(parent)
+{
+    // ...
+}
+
+
+GeoImageModel::~GeoImageModel()
+{
+    // ...
+}
+
+
+void GeoImageModel::insert ( const GeoImage* item )
+{
+    beginInsertRows(QModelIndex(), images.count(), images.count() + 1 );
+    images.append(item);
+    endInsertRows();
+}
+
+
+QVariant GeoImageModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (role != Qt::DisplayRole)
+    {
+        return QVariant();
+    }
+    if (orientation == Qt::Horizontal)
+    {
+        return QString("Column %1").arg(section);
+    }
+    else{
+        return QString("Image %1").arg(section);
+    }
+}
+
+
+int GeoImageModel::rowCount(const QModelIndex&) const
+{
+    return images.count();
+}
+
+
+QVariant GeoImageModel::data(const QModelIndex & index, int role) const
+{
+    if (!index.isValid())
+    {
+        return QVariant();
+    }
+    if (index.row() < 0 || index.row() >= images.size())
+    {
+        return QVariant();
+    }
+    if (role == Qt::DisplayRole)
+    {
+        return images.at(index.row())->name();
+    }
+    else
+    {
+        return QVariant();
+    }
+}
+
+
+const GeoImageModel* GeoImageModel::data(int index) const
+{
+    Q_ASSERT(index >=0 && index < images.size());
+    return images.at(index);
+}
---
+++ Qeo/GeoImageModel.h
@@ -0,0 +1,31 @@
+// $Id$
+// Copyright (c) 2008 Oliver Lau <oliver@ersatzworld.net>
+// Alle Rechte vorbehalten.
+
+#ifndef __GEOIMAGEMODEL_H_
+#define __GEOIMAGEMODEL_H_
+
+#include <QAbstractListModel>
+#include <QList>
+
+class GeoImage;
+
+class GeoImageModel : public QAbstractListModel
+{
+    Q_OBJECT
+
+public:
+    GeoImageModel(QObject *parent);
+    ~GeoImageModel();
+
+    void insert(const GeoImage* item);
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
+    int rowCount(const QModelIndex& parent = QModelIndex()) const;
+    const GeoImage* data(int index) const;
+    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const;
+
+private:
+    QList<const GeoImage*> images;
+};
+
+#endif // __GEOIMAGEMODEL_H_
