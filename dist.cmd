bb0abbf19fd028f2287d32f84af82e1913e5fd95
Oliver Lau <oliver@ersatzworld.net>
2008-09-24 10:00:19 -0100

    Distributionshelferlein fur Windows neu.

---
+++ dist.cmd
@@ -0,0 +1,8 @@
+@SET DSTDIR=gpstools
+@SET TOOLDIR=win32-tools
+@IF NOT EXIST %DSTDIR% MKDIR %DSTDIR%
+@ECHO Kopieren der EXE-Dateien ...
+@FOR %%F IN (areameter gpsplot gpxtimeshift splittrack trkcompare wpl1000reader) DO @COPY /B /Y /V Release\%%F.exe %DSTDIR%\
+@ECHO Zip'pen der EXE-Dateien in %DSTDIR% ...
+@%TOOLDIR%\zip.exe -9 -f gpstools.zip %DSTDIR%\*
+@%TOOLDIR%\unzip.exe -t -q gpstools.zip
