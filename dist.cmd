81529986a1dd1b14cf2bd2ddc6488abfdaa3da8a
Oliver Lau <oliver@ersatzworld.net>
2008-09-24 12:07:06 -0100

    dist.cmd korrigiert.

--- dist.cmd
+++ dist.cmd
@@ -1,11 +1,10 @@
 @SET DSTDIR=gpstools
-@SET ZIPFILE=gpstools
-@IF NOT "%1" == "" @SET ZIPFILE=%ZIPFILE%-
+@SET ZIPFILE=gpstools-win32.zip
 @SET TOOLDIR=win32-tools
 @IF NOT EXIST %DSTDIR% MKDIR %DSTDIR%
 @ECHO Kopieren der EXE-Dateien ...
 @FOR %%F IN (areameter gpsplot gpxtimeshift splittrack trkcompare wpl1000reader) DO @COPY /B /Y /V Release\%%F.exe %DSTDIR%\
 @COPY /Y LICENSE.txt %DSTDIR%
 @ECHO Zip'pen der EXE-Dateien in %DSTDIR% ...
-@%TOOLDIR%\zip.exe -9 gpstools.zip %DSTDIR%\*
-@%TOOLDIR%\unzip.exe -t -q gpstools.zip
+@%TOOLDIR%\zip.exe -9 %ZIPFILE% %DSTDIR%\*
+@%TOOLDIR%\unzip.exe -t -q %ZIPFILE%
