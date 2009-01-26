ca91c3af3d9efab7e5918583597191238655ba5b
Oliver Lau <oliver@ersatzworld.net>
2009-01-26 14:42:35 -0100

    wpl1000: Code aufger?umt.

--- wpl1000/stdafx.cpp
+++ wpl1000/stdafx.cpp
@@ -1,8 +1,4 @@
-// stdafx.cpp : source file that includes just the standard includes
-// wpl1000.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
+// $Id$
+// Copyright (c) 2009 Oliver Lau <oliver@ersatzworld.net>
 
 #include "stdafx.h"
-
-// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file
--- wpl1000/stdafx.h
+++ wpl1000/stdafx.h
@@ -1,7 +1,5 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
+// $Id$
+// Copyright (c) 2009 Oliver Lau <oliver@ersatzworld.net>
 
 #pragma once
 
