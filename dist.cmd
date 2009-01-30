11722bf7d8e81fd807dde8182e0c6f6ae3f1bb19
Oliver Lau <oliver@ersatzworld.net>
2009-01-30 07:17:53 +0100

    dist.cmd um wpl1000 erweitert.

--- dist.cmd
+++ dist.cmd
@@ -3,7 +3,7 @@
 @SET TOOLDIR=win32-tools
 @IF NOT EXIST %DSTDIR% MKDIR %DSTDIR%
 @ECHO Kopieren der EXE-Dateien ...
-@FOR %%F IN (areameter gpsplot gpxtimeshift splittrack trkcompare wpl1000reader) DO @COPY /B /Y /V Release\%%F.exe %DSTDIR%\
+@FOR %%F IN (areameter gpsplot gpxtimeshift splittrack trkcompare wpl1000reader wpl1000) DO @COPY /B /Y /V Release\%%F.exe %DSTDIR%\
 @COPY /Y LICENSE.txt %DSTDIR%
 @ECHO Zip'pen der EXE-Dateien in %DSTDIR% ...
 @%TOOLDIR%\zip.exe -9 %ZIPFILE% %DSTDIR%\*
